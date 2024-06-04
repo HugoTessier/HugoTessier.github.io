0bbbfbfe77d13ff7295df7deccb9e46610c1f3e5
htessier <tessier.hug@gmail.com>
2024-06-04 11:08:34 -0400

    Now possible to split nodes into different folders

--- create_all_nodes_list.py
+++ create_all_nodes_list.py
@@ -2,7 +2,20 @@ import os
 import json
 
 if __name__ == '__main__':
-    path = './nodes'
-    filenames = {'files': [os.path.join(path, f) for f in os.listdir(path)]}
+    filenames = {'files': []}
+
+
+    def parse_folder(path):
+        for f in os.listdir(path):
+            file_path = os.path.join(path, f)
+            if os.path.isdir(file_path):
+                parse_folder(file_path)
+            elif file_path.endswith('.json'):
+                filenames['files'].append(file_path)
+            else:
+                pass
+
+
+    parse_folder('./nodes')
     with open('./all_nodes.json', 'w') as f:
         json.dump(filenames, f)
